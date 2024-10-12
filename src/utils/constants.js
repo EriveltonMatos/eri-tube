26a553b55f4954bb5f03ae0c6658d49aba2194e6
Erivelton Matos <eriveltonmatos@hotmail.com>
2024-10-12 11:58:13 -0300

    final version

---
+++ src/utils/constants.js
@@ -0,0 +1,28 @@
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import GamesIcon from '@mui/icons-material/Games';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+
+
+export const logo = './images/eri-tube.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Relaxing Music', icon: <LibraryMusicIcon />, },
+  { name: 'New Javascript', icon: <CodeIcon />, },
+  { name: 'Java', icon: <CodeIcon />, },
+  { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'React.js', icon: <CodeIcon />, },
+  { name: 'Spring', icon: <CodeIcon />, },
+  { name: 'Lo-Fi', icon: <LibraryMusicIcon /> },
+  { name: 'PC Games', icon: <GamesIcon />, },
+  { name: 'Post Punk', icon: <LibraryMusicIcon /> },
+  { name: 'Heavy Metal', icon: <LibraryMusicIcon /> },
+  { name: 'Power Metal', icon: <LibraryMusicIcon /> },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = '';
+export const demoVideoTitle = '';
